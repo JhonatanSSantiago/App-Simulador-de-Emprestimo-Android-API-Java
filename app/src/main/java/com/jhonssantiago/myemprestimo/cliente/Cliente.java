40e15367fdb9836d80a77266440729c3d0fd9beb
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-02-21 12:28:09 -0300

    Criação classe Cliente

---
+++ app/src/main/java/com/jhonssantiago/myemprestimo/cliente/Cliente.java
@@ -0,0 +1,32 @@
+package com.jhonssantiago.myemprestimo.cliente;
+
+public class Cliente {
+    private String nome;
+    private double salario;
+
+    public Cliente(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "nome: " + nome  + ", salario: " + salario ;
+    }
+}
