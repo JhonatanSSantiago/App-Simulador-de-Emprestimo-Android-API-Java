4010531872ee3384da627328e65815dcb11e432d
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-02-28 17:59:42 -0300

    finalizado sem ListView

--- app/src/main/java/com/jhonssantiago/myemprestimo/MainActivity.java
+++ app/src/main/java/com/jhonssantiago/myemprestimo/MainActivity.java
@@ -2,6 +2,7 @@ package com.jhonssantiago.myemprestimo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jhonssantiago.myemprestimo.cliente.Cliente;
+import com.jhonssantiago.myemprestimo.emprestimo.Emprestimo;
 
 /*
 O app deve receber o valor do empréstimo, quantidade de parcelas, taxa de juros e salário do cliente.
-Saiba que, o empréstimo só pode ser concedido se o valor da parcela for menor ou igual a 30% do salário do cliente.
-Você deve utilizar o cálculo de juros compostos, por exemplo, em um empréstimo no valor de R$ 10000,00 em 10 parcelas,
+Saiba que, o empréstimo só pode ser concedido se o valor da parcela for menor ou igual a 30% do salário do cliente. - OK
+Você deve utilizar o cálculo de juros compostos, por exemplo, em um empréstimo no valor de R$ 1000,00 em 10 parcelas,
 se o banco utiliza uma taxa de juros de 10% ao mês, o valor total da dívida a ser pago no 1º mês será de R$ 1.100,00
 (R$ 1.000,00 de empréstimo, acrescidos de R$ 100,00 que são 10% da quantia do empréstimo).
 Porém, no 2º mês, os juros serão cobrados em cima dos R$ 1.100,00 cobrados no mês anterior,
@@ -26,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private Button calcular;
     private EditText nometxt, salariotxt, jurostxt, parcelastxt, emprestimotxt;
     private String nome;
-    private double salario, valorParcela, porcentagem;
-    private int parcela, emprestimo, juros;
+    private double salario, valorParcela, porcentagem, valorEmprestimo,  jurosMes;
+    private int qtdParcela;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +46,17 @@ public class MainActivity extends AppCompatActivity {
                 converterDados();
                 Cliente c = new Cliente(nome, salario);
                 Toast.makeText(getApplicationContext(), "cliente: "+ c.toString(), Toast.LENGTH_SHORT).show();
-                calcularParcela(emprestimo, parcela);
+                calcularParcela(valorEmprestimo, qtdParcela);
                 calulcarPorcentagem(salario, valorParcela);
               //  Toast.makeText(getApplicationContext(), "%: "+ porc, Toast.LENGTH_LONG).show();
-
                 if(porcentagem < 30){
-                    Toast.makeText(getApplicationContext(), "Emprestimo: valor total: "+emprestimo+" qtd parcelas: "+parcela+" valor parcela: "+valorParcela+ "porcentagm: "+porcentagem, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Emprestimo: valor total: "+valorEmprestimo+" qtd parcelas: "+qtdParcela+" valor parcela: "+valorParcela+ "juros de : "+jurosMes, Toast.LENGTH_LONG).show();
+                    Intent it = new Intent(getApplicationContext(), PropostaActivity.class);
+                    it.putExtra("dado_valorEmprestimo", valorEmprestimo);
+                    it.putExtra("dado_jurosMes", jurosMes);
+                    it.putExtra("dado_qdtParcela", qtdParcela);
+                    it.putExtra("dado_valorParcela", valorParcela);
+                    startActivity(it);
                 }else{
                     Toast.makeText(getApplicationContext(), "Não podemos, porcentagem: "+porcentagem, Toast.LENGTH_LONG).show();
                 }
@@ -65,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
     private void converterDados() {
         nome = nometxt.getText().toString();
         salario = Double.parseDouble(salariotxt.getText().toString());
-        parcela = Integer.parseInt(parcelastxt.getText().toString());
-        emprestimo =  Integer.parseInt(emprestimotxt.getText().toString());
-        juros =  Integer.parseInt(jurostxt.getText().toString());
+        qtdParcela = Integer.parseInt(parcelastxt.getText().toString());
+        valorEmprestimo =  Double.parseDouble(emprestimotxt.getText().toString());
+        jurosMes =  Double.parseDouble(jurostxt.getText().toString());
     }
 
     private void calcularParcela(double emprestimo, int parcela) {
